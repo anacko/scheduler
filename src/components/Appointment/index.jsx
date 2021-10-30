821d723c9b22ee81aa242b2bc9d5d421dd8f4ffb
Ana Carolina K <anacko@gmail.com>
2021-10-30 18:47:12 -0400

    add custom hook to manage appointments rendering

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -3,6 +3,7 @@ import './styles.scss';
 import Header from './Header';
 import Empty from './Empty';
 import Show from './Show';
+import {useVisualMode} from '../hooks/useVisualMode';
 
 export default function Appointment (props) {
   console.log('props in Appointment', props)
