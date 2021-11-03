d7b8ed1efa889af9a27da247fb9f8effa1580f09
Ana Carolina K <anacko@gmail.com>
2021-11-03 19:03:48 -0400

    add user tests

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -20,7 +20,7 @@ export default function DayListItem(props) {
   }
 
   return (
-    <li className={dayListItemClass} onClick={handleClick}>
+    <li className={dayListItemClass} onClick={handleClick} data-testid="day" >
       <h2 className="text--regular">{props.name}</h2> 
       <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
